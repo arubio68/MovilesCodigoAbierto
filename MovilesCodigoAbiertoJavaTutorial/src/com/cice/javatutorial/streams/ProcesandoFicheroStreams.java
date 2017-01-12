17fcab509067961c9949d7896ade01bb05caec67
luis <luis_A_serrano@msn.com>
2017-01-12 13:50:45 +0100

    new updates

--- MovilesCodigoAbiertoJavaTutorial/src/com/cice/javatutorial/streams/ProcesandoFicheroStreams.java
+++ MovilesCodigoAbiertoJavaTutorial/src/com/cice/javatutorial/streams/ProcesandoFicheroStreams.java
@@ -15,7 +15,12 @@ public class ProcesandoFicheroStreams {
 			//Stream str = Files.lines(Paths.get("palabras2"));
 			//str.forEach(line->System.out.println("line:"+ line));
 			// usando lambda
-			Files.lines(Paths.get("palabras2")).forEach(line->System.out.println("line:"+ line));
+			//Files.lines(Paths.get("palabras2")).forEach(line->System.out.println("line:"+ line));
+			// otro ejemplo 
+			Files.lines(Paths.get("palabras2"))
+			.filter(line->line.length()<10)
+			.forEach(line->System.out.println(line));
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
